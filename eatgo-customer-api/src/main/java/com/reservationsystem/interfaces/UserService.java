85526b1ccb268b7c769f18ea3444dca5da625e78
KIM-JS-95 <baugh248730@gmail.com>
2021-03-27 00:22:25 +0900

    2021-03-27

---
+++ eatgo-customer-api/src/main/java/com/reservationsystem/interfaces/UserService.java
@@ -0,0 +1,12 @@
+package com.reservationsystem.interfaces;
+
+import com.reservationsystem.domain.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public User registerUser(String email, String name, String password) {
+        return null;
+    }
+}
